148dcd7cfcfe616635242c4f5ac2449ce83a8d3d
yanyanyaa <yclin3411@gmail.com>
2023-07-30 22:52:33 +0800

    feat: add express-session; add login feature via passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -7,11 +8,27 @@ const PORT = 3000
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 app.get('/', (req, res) => {
   return Todo.findAll({
