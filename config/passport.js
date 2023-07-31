e508bdeee64564c0773523fa2de01ca6c4a17e7f
yanyanyaa <yclin3411@gmail.com>
2023-07-31 16:22:54 +0800

    feat: add FB login feature(not done)

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,13 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
+  // 本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
@@ -21,6 +23,31 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
+  // FB 登入策略
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
